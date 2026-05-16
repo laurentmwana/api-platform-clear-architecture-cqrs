85ddb7450d7edfed18472508a7556f0dfc6445c0
Labeya <laurentmwn@gmail.com>
2026-05-16 21:48:17 +0100

    application(logout): implement logout command and handler
    - Add LogoutCommand with user, ip and userAgent
    - Create LogoutHandler using AccessTokenManager
    - Invalidate JWT token and mark session for logout

---
+++ src/IdentityAndAccess/Application/CommandHandler/LogoutHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\IdentityAndAccess\Application\CommandHandler;
+
+use App\IdentityAndAccess\Application\Command\LogoutCommand;
+use App\IdentityAndAccess\Domain\Exception\InvalidJwtTokenException;
+use App\IdentityAndAccess\Domain\Repository\SessionRepository;
+use App\IdentityAndAccess\Domain\Service\AccessTokenManager;
+use App\SharedContext\Application\Bus\Command\CommandHandler;
+
+final class LogoutHandler implements CommandHandler
+{
+   public function __construct(
+      private AccessTokenManager $accessTokenManager,
+      private SessionRepository $sessionRepository
+   ) {}
+
+   public function __invoke(LogoutCommand $command): void
+   {
+      $isOk = $this->accessTokenManager
+         ->invalidate($command->getJwtToken());
+
+      if (!$isOk) {
+         throw new InvalidJwtTokenException();
+      }
+
+
+      $user = $command->getUser();
+      $ipAddress = $command->getIpAddress();
+      $userAgent = $command->getUserAgent();
+
+      if ($userAgent !== null && $ipAddress !== null) {
+
+         $session = $this->sessionRepository
+            ->findByUserIdAndDevice(
+               $user->getId(),
+               $ipAddress,
+               $userAgent
+            );
+
+         if ($session !== null) {
+            $session->markForLogout();
+
+            $this->sessionRepository->save($session);
+         }
+      }
+   }
+}
